f86668016a5623c0700bbf6f119489711f73cccb
Esteves Tiago <estti379@gmail.com>
2018-10-19 16:49:31 +0200

    Commit week 1
    Hello world document created

---
+++ Week1/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,28 @@
+public class HelloWorld {
+
+    public static void main(String[] args)
+    {
+
+
+
+
+        System.out.print("Hello, ");
+        System.out.println("it's me, \"Gonçalo\"! \\n");
+        if () {
+
+        } else {
+
+        }
+            while
+        for (int i = 0; i < ; i++) {
+
+        }
+            do {
+
+            }while
+
+
+    }
+
+
+}
